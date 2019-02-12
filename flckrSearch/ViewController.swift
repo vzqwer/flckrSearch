b675307fd87369cbfbed27b0a7ce611440b58fef
Oleg Shulakov <shulakov_oleg@mail.ru>
2019-02-12 21:32:26 +0400

    Initial Commit

---
+++ flckrSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flckrSearch
+//
+//  Created by Oleg Shulakov on 12.02.2019.
+//  Copyright © 2019 Oleg Shulakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
